66c50a99c19ddf8ec136e15d18302e7427d2b5e1
demasialol <demasialol@naver.com>
2021-04-29 15:46:24 +0900

    Time elapsed

--- app/src/main/java/com/androidapp/hellokotlin/VariableJavaActivity.java
+++ app/src/main/java/com/androidapp/hellokotlin/VariableJavaActivity.java
@@ -13,9 +13,9 @@ import java.util.Locale;
 public class VariableJavaActivity extends AppCompatActivity {
     TextView txtActivityStartTime, txtCountBtnClicks, txtElapsedTime;
     Button btnClickMe;
-    long elapsedSeconds;
     int clcikCount;
-    final long startTime= System.currentTimeMillis();
+    long elapsedSeconds;
+    long startTime= System.currentTimeMillis();;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,20 +25,20 @@ public class VariableJavaActivity extends AppCompatActivity {
         txtCountBtnClicks = findViewById(R.id.txtCountBtnClicks);
         txtElapsedTime = findViewById(R.id.txtElapsedTime);
         btnClickMe = findViewById(R.id.btnClickMe);
+
         clcikCount=0;
         long elapsedSeconds=0;
         long startTime= System.currentTimeMillis();
 
+
         btnClickMe.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //var elapsedSeconds : Long = ((System.currentTimeMillis()-startTime)/1000.0).toLong()
-
-                //elapsedSeconds = ((System.currentTimeMillis()-startTime)/1000.0);
                 long elapsedSeconds = (long) ((System.currentTimeMillis()-startTime)/1000.0);
                 clcikCount++;
                 txtCountBtnClicks.setText("Button clicks = " +clcikCount);
-                txtElapsedTime.setText("Elapsed Seconds ="+elapsedSeconds);
+                txtElapsedTime.setText("Elapsed Seconds =" + elapsedSeconds);
+
             }
         });
         String sTimeStamp = new SimpleDateFormat("HH:mm:ss", Locale.KOREA).format(startTime);
