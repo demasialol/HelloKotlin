4a1794543f79b33289d8cb35f3a2fe8f363891d2
demasialol <demasialol@naver.com>
2021-04-29 14:47:51 +0900

    Kotlin practice

---
+++ app/src/main/java/com/androidapp/hellokotlin/VariableKotlinActivity.kt
@@ -0,0 +1,34 @@
+package com.androidapp.hellokotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import java.text.SimpleDateFormat
+import java.util.*
+
+class VariableKotlinActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.layout_variable)
+
+        val txtActivityStartTime = findViewById<TextView>(R.id.txtActivityStartTime)
+        val txtCountBtnCLicks = findViewById<TextView>(R.id.txtCountBtnClicks)
+        val btnClickMe = findViewById<Button>(R.id.btnClickMe)
+
+        var clickCount = 0
+        val startTIme = System.currentTimeMillis()
+        val sTimeStamp = SimpleDateFormat("HH:mm:ss", Locale.KOREA).format(startTIme)
+
+        txtActivityStartTime.text="Activity start time = ${sTimeStamp}"
+
+        btnClickMe.setOnClickListener {
+            clickCount++
+            txtCountBtnCLicks.text = "Button clicks = ${clickCount}"
+
+
+        }
+
+
+    }
+}
